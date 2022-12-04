81b88b9f4e2509ecad17fb15ec222bb1df7543e1
Sohel Samimi <47162719+SohelSamim@users.noreply.github.com>
2022-12-04 22:11:51 +0100

    Update index.js
    Deleted my comments

--- server/index.js
+++ server/index.js
@@ -4,32 +4,20 @@ const path = require("path");
 const app = express();
 const cors = require("cors");
 
-// this is to get data from the api from front end
 app.use(express.json());
-
-// adding cors
 app.use(cors());
-
-// post api for search result
 
 app.post("/", (req, res) => {
   fs.readFile(path.join(__dirname, "flights.json"), "utf-8", (err, data) => {
-    // this is the data i am getting from the frontend as a request
     let DeparterDestination = req.body.DeparterDestination;
     let ArrivalDestination = req.body.ArrivalDestination;
     let DeparterDate = req.body.DeparterDate.split("T")[0];
     let ArrivalDate = req.body.ArrivalDate.split("T")[0];
     let seats = req.body.seats;
-
-    // this is the data i am getting from the file as a request
     let DataInJson = JSON.parse(data);
-
-    // array for the fiter
     let FilterArray = [];
-
-    // now filter algorithm start
+    
     DataInJson.forEach((Eachelement) => {
-      // first ill check available destination equality
       if (
         Eachelement.depatureDestination == DeparterDestination &&
         Eachelement.arrivalDestination == ArrivalDestination
@@ -37,15 +25,11 @@ app.post("/", (req, res) => {
         Eachelement["itineraries"].forEach((Eachitineraries) => {
           let depatureAt = Eachitineraries.depatureAt.split("T")[0];
           let ArrivalAt = Eachitineraries.arriveAt.split("T")[0];
-
-          // then ill check available DeparterDate and ArrivalDate and seats
           if (
             DeparterDate == depatureAt &&
             ArrivalDate == ArrivalAt &&
             Eachitineraries.avaliableSeats >= seats
           ) {
-            // now ill push
-            // i am pusing id,route,daparture date formate and arrival date formate and after that complete object so that i can work easilty on front end
             FilterArray.push({
               id: Eachelement.flight_id,
               route: `${DeparterDestination} To ${ArrivalDestination}`,
@@ -62,27 +46,20 @@ app.post("/", (req, res) => {
         });
       }
     });
-
-    // now send to the front end the filter result
+    
     res.send(FilterArray);
   });
 });
-// this is for update the seats
+
 app.post("/update-seats", (req, res) => {
-  // getting data for filtering purpose
   fs.readFile(path.join(__dirname, "flights.json"), "utf-8", (err, data) => {
     let id = req.body.id;
-
-    // converting into json
-
     let DataInJson = JSON.parse(data);
     let FilterArray = [];
-    // filtering process start
     DataInJson.forEach((Eachelement) => {
-      // first check flight id if true go on
+      
       if (Eachelement.flight_id == id) {
         Eachelement["itineraries"].forEach((Eachitineraries) => {
-          // then check and date and time of departure
           if (
             Eachitineraries.depatureAt.split("T")[0] ==
               req.body.depatureAt.split("T")[0] &&
@@ -98,10 +75,7 @@ app.post("/update-seats", (req, res) => {
       FilterArray.push(Eachelement);
     });
 
-    // after filter convert into string
     let FilterJSon = JSON.stringify(FilterArray);
-
-    // then pass into the flights.json file
     fs.writeFile("flights.json", FilterJSon, (err) => {
       if (!err) {
         res.send({ message: "success Update Done" });
